7624493d3bb6b5a105b023f4fd151c1394a18ca3
Jason Rebuck <jaschon@users.noreply.github.com>
2023-11-19 17:08:50 -0800

    Add files via upload

---
+++ lua/fizzbuzz.lua
@@ -0,0 +1,15 @@
+#!/usr/bin/lua
+
+local i = 1
+while i < 101 do
+  if i % 3 == 0 and i % 5 == 0 then
+      print "FIZZBUZZ"
+  elseif  i % 3 == 0 then
+      print("FIZZ")
+  elseif i % 5 == 0 then
+      print("BUZZ")
+  else
+      print(i)
+  end
+    i = i + 1
+end
