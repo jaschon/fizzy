a02adc7373a331e2818d6add0af360acf4feb663
Jason Rebuck <jaschon@users.noreply.github.com>
2023-11-19 16:31:33 -0800

    Add files via upload

---
+++ ruby/fizzbuzz-classic.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+(1..100).each do |i|
+  if i%3 ==0 && i%5 == 0
+    puts "FIZZBUZZ"
+  elsif i%3 == 0
+    puts "FIZZ"
+  elsif i%5 == 0
+    puts "BUZZ"
+  else
+    puts i
+  end
+end
