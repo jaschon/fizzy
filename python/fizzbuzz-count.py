65f021e7bad938fa11f5fa3046dc16d588a22187
Jason Rebuck <jaschon@users.noreply.github.com>
2023-11-19 16:19:03 -0800

    Add files via upload

---
+++ python/fizzbuzz-count.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+threes, fives = 1,1
+for i in range(1,101):
+    match (threes, fives):
+        case (3,5):
+            print("FIZZBUZZ")
+            threes, fives = 0,0 
+        case (3, _):
+            print("FIZZ")
+            threes = 0
+        case (_, 5):
+            print("BUZZ")
+            fives = 0
+        case _:
+            print(i)
+    threes += 1 
+    fives += 1
+
+
+
